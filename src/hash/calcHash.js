eec64dbaf88fb2b88f15c46b2ed0665069387f95
Mikhail Matsuev <mikhail.matsuev@dreamappstudio.com>
2025-10-24 19:14:24 +0300

    hash completed

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,32 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-  // Write your code here
+    const _filename = fileURLToPath(import.meta.url);
+    const _dirname = dirname(_filename);
+    const fileNameToCalculateHashFor = 'fileToCalculateHashFor.txt';
+    const filePath = join(_dirname, 'files', fileNameToCalculateHashFor);
+
+    return new Promise((res, rej) => {
+        const hash = createHash('sha256');
+        const stream = createReadStream(filePath);
+
+        stream.on('data', (chunk) => {
+            hash.update(chunk);
+        });
+
+        stream.on('end', () => {
+            const hexHash = hash.digest('hex');
+            console.log(hexHash);
+            res(hexHash);
+        });
+
+        stream.on('error', (error) => {
+            rej(error);
+        });
+    });
 };
 
 await calculateHash();
