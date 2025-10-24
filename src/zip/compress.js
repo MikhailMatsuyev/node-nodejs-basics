2820c63563c0b3b6bafe6ba11b0ac220545d4cfd
Mikhail Matsuev <mikhail.matsuev@dreamappstudio.com>
2025-10-24 19:22:19 +0300

    zip completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,48 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const compress = async () => {
-  // Write your code here
+    const _filename = fileURLToPath(import.meta.url);
+    const _dirname = dirname(_filename);
+
+    const sourceFileName = 'fileToCompress.txt';
+    const sourceFile = join(_dirname, 'files', sourceFileName);
+
+    const archiveFileName = 'archive.gz';
+    const archiveFile = join(_dirname, 'files', archiveFileName);
+
+    return new Promise((resolve, reject) => {
+        const readStrm = createReadStream(sourceFile);
+        const writeStrm = createWriteStream(archiveFile);
+        const gzip = createGzip();
+
+        let tBytes = 0;
+        let comprBytes = 0;
+
+        readStrm.on('data', (chunk) => {
+            tBytes += chunk.length;
+        });
+
+        writeStrm.on('data', (chunk) => {
+            comprBytes += chunk.length;
+        });
+
+        readStrm
+            .pipe(gzip)
+            .pipe(writeStrm)
+            .on('finish', () => {
+                console.log(`Compress finished!`);
+                console.log(`1st size: ${tBytes} bytes`);
+                console.log(`After compressing: ${comprBytes} bytes`);
+                resolve();
+            })
+            .on('error', (error) => {
+                console.error('Error:', error.message);
+                reject(error);
+            });
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { createGunzip } from 'zlib';
+import { pipeline } from 'stream/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 const decompress = async () => {
-  // Write your code here
+    const _filename = fileURLToPath(import.meta.url);
+    const _dirname = dirname(_filename);
+
+    const archFileName = 'archive.gz'
+    const archFile = join(_dirname, 'files', archFileName);
+    const outFileName = 'fileToCompress.txt';
+    const outFile = join(_dirname, 'files', outFileName);
+
+    try {
+        await pipeline(
+            createReadStream(archFile),
+            createGunzip(),
+            createWriteStream(outFile)
+        );
+        console.log(`Decompressed to ${outFile}`);
+    } catch (error) {
+        console.error('Error:', error.message);
+        throw error;
+    }
 };
 
 await decompress();
