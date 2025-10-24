322a478a1070398ed8b893660639235b4cccfcb3
Mikhail Matsuev <mikhail.matsuev@dreamappstudio.com>
2025-10-24 19:07:40 +0300

    cli completed

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-  // Write your code here
+    const envV = process.env;
+    const rssV = [];
+    for (const key in envV) {
+        if (key.startsWith('RSS_')) {
+            rssV.push(`${key}=${envV[key]}`);
+        }
+    }
+    console.log(rssV.join('; '));
 };
 
 parseEnv();
